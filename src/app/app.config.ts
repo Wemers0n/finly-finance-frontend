fe32c0e3afbbe9d6c833792cdaa44ba681cfe98a
Wemerson <nerdhits001@gmail.com>
2026-03-24 12:27:24 -0300

    Initial Commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, LOCALE_ID } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+registerLocaleData(localePt);
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch())
+  ]
+};
